2780bb7f2d4d525483130abda45dd327dfaba247
masudrana5 <abdullainfo30@gmail.com>
2023-04-15 19:35:57 +0600

    videos get and rtk query setup and single video get is done

--- src/components/pages/Video.js
+++ src/components/pages/Video.js
@@ -1,20 +1,53 @@
+import { useParams } from "react-router-dom";
 import Description from "../video/Description";
 import Player from "../video/Player";
 import RelatedVideos from "../video/related/RelatedVideos";
+import { useGetVideoQuery } from "../../features/api/apiSlice";
+import PlayerLoader from "../ui/loaders/PlayerLoader";
+import DescriptionLoader from "../ui/loaders/DescriptionLoader";
+import Error from "../ui/Error";
+import RelatedVideoLoader from "../ui/loaders/RelatedVideoLoader";
 
 export default function Video() {
-    return (
-        <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
-            <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
-                <div className="grid grid-cols-3 gap-2 lg:gap-8">
-                    <div className="col-span-full w-full space-y-8 lg:col-span-2">
-                        <Player />
-                        <Description />
-                    </div>
-
-                    <RelatedVideos />
-                </div>
-            </div>
-        </section>
+  const { videoId } = useParams();
+  const { data: video, isLoading, isError } = useGetVideoQuery(videoId);
+  let content = null;
+  if (isLoading)
+    content = (
+      <>
+        <PlayerLoader />
+        <DescriptionLoader />
+      </>
     );
+  if (!isLoading && isError) content = <Error message="There was an error!" />;
+  if (!isError && !isError && video?.id)
+    content = (
+      <>
+        <Player link={video?.link} title={video?.title} />
+        <Description video={video} />
+      </>
+    );
+  return (
+    <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
+      <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
+        <div className="grid grid-cols-3 gap-2 lg:gap-8">
+          <div className="col-span-full w-full space-y-8 lg:col-span-2">
+            {content}
+          </div>
+
+          {video?.id ? (
+            <RelatedVideos />
+          ) : isLoading ? (
+            <>
+              <RelatedVideoLoader />
+              <RelatedVideoLoader />
+              <RelatedVideoLoader />
+            </>
+          ) : (
+            <Error message="There was an Error!" />
+          )}
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,25 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
+  const { data: videos, isLoading, isError } = useGetVideosQuery();
+  let content = null;
+  if (isLoading)
+    content = (
+      <>
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+        <VideoLoader />
+      </>
     );
+  if (!isLoading && isError)
+    content = <Error message="There was an error ocurren" />;
+  if (!isLoading && !isError && videos?.length === 0)
+    content = <Error message="video no found!" />;
+  if (!isLoading && !isError && videos?.length > 0)
+    content = videos?.map((video) => <Video key={video.id} video={video} />);
+  return content;
 }
