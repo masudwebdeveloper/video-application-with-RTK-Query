73d5cacf4dd3f074c3454bbc84f7fac8ea888c4c
masudrana5 <abdullainfo30@gmail.com>
2023-04-19 23:34:15 +0600

    new commit

--- src/components/add/AddVideo.js
+++ src/components/add/AddVideo.js
@@ -3,6 +3,7 @@ import Form from "./Form";
 export default function AddVideo() {
     return (
         <div className="max-w-7xl mx-auto px-5 lg:px-0">
+            {/* this below add button */}
             <div className="w-full">
                 <div className="px-4 sm:px-0 pb-4">
                     <h3 className="text-lg font-medium leading-6 text-gray-900">
@@ -11,8 +12,8 @@ export default function AddVideo() {
                     <p className="mt-1 text-sm text-gray-600">
                         Please fillup the form to add new video
                     </p>
-                {/* </div> */}
-                this is form
+                </div>
+                {/* this is form */}
                 <div className="mt-5 md:mt-0 md:col-span-2">
                     <Form />
                 </div>
